91762e4366b530e028891dfd2f85c5a5ad3d7fdb
Antoine TAOURIRT <>
2019-06-18 15:40:01 +0200

    First commit

---
+++ WeTrot/WeTrot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeTrot
+//
+//  Created by Antoine TAOURIRT on 18/06/2019.
+//  Copyright © 2019 WeSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
